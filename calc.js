7f1b8ab3d6779fd2b436deec499996f43ee0c003
JMestre32 <114640505+JMestre32@users.noreply.github.com>
2024-10-01 11:17:03 -0700

    Added event listeners

--- calc.js
+++ calc.js
@@ -32,6 +32,29 @@ function operate(left, operand, right){
 
 const display = document.querySelector(".display")
 
+const zero = document.querySelector("#zero")
+zero.onclick = () => display.textContent = display.textContent.concat(zero.textContent)
+const one = document.querySelector("#one")
+one.onclick = () => display.textContent = display.textContent.concat(one.textContent)
+const two = document.querySelector("#two")
+two.onclick = () => display.textContent = display.textContent.concat(two.textContent)
+const three = document.querySelector("#three")
+three.onclick = () => display.textContent = display.textContent.concat(three.textContent)
+const four = document.querySelector("#four")
+four.onclick = () => display.textContent = display.textContent.concat(four.textContent)
+const five = document.querySelector("#five")
+five.onclick = () => display.textContent = display.textContent.concat(five.textContent)
+const six = document.querySelector("#six")
+six.onclick = () => display.textContent = display.textContent.concat(six.textContent)
+const seven = document.querySelector("#seven")
+seven.onclick = () => display.textContent = display.textContent.concat(seven.textContent)
+const eight = document.querySelector("#eight")
+eight.onclick = () => display.textContent = display.textContent.concat(eight.textContent)
+const nine = document.querySelector("#nine")
+nine.onclick = () => display.textContent = display.textContent.concat(nine.textContent)
+const dot = document.querySelector("#dot")
+dot.onclick = () => display.textContent = display.textContent.concat(dot.textContent)
 
-const svn = document.querySelector("#seven")
-svn.onclick = () => display.textContent = "7"
+
+const clear = document.querySelector(".clear")
+clear.onclick = () => display.textContent = ""
